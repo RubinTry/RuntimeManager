e877fbbe636d56b87431ae46782011a6937c9baa
rubintry <13857769302@163.com>
2022-03-14 19:28:12 +0800

    移除了有冲突的依赖

--- RtManager/src/main/java/cn/rubintry/rtmanager/core/MainIconView.kt
+++ RtManager/src/main/java/cn/rubintry/rtmanager/core/MainIconView.kt
@@ -4,13 +4,13 @@ package cn.rubintry.rtmanager.core
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.core.content.ContextCompat
-import androidx.core.graphics.drawable.toBitmap
 import androidx.customview.widget.ViewDragHelper
 import cn.rubintry.rtmanager.R
 import com.blankj.utilcode.util.ImageUtils
@@ -43,9 +43,12 @@ class MainIconView : FrameLayout , IRuntimeView {
         this.id = R.id.runtime_contentview_id
         imgMainIcon = mRootView?.findViewById(R.id.img_main_icon)
         val bgDrawable = ContextCompat.getDrawable(context , R.mipmap.icon_tools)
-        var bitmap = ImageUtils.toRound(bgDrawable?.toBitmap(bgDrawable.intrinsicWidth, bgDrawable.intrinsicHeight , Bitmap.Config.ARGB_8888))
-        bitmap = ImageUtils.addCircleBorder(bitmap , SizeUtils.dp2px(7f).toFloat() , Color.parseColor("#C1C1C1"))
-        imgMainIcon?.setImageBitmap(bitmap)
+        if(bgDrawable is BitmapDrawable){
+            val mBitmap = Bitmap.createScaledBitmap(bgDrawable.bitmap , bgDrawable.intrinsicWidth , bgDrawable.intrinsicHeight , true)
+            var bitmap = ImageUtils.toRound(mBitmap)
+            bitmap = ImageUtils.addCircleBorder(bitmap , SizeUtils.dp2px(7f).toFloat() , Color.parseColor("#C1C1C1"))
+            imgMainIcon?.setImageBitmap(bitmap)
+        }
     }
 
     fun setViewDragHelper(helper: ViewDragHelper) {
